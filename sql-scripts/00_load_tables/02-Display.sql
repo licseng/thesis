cf30c213164b0068461097fa122c7b928fb9c4dc
licseng <kekvirag94@gmail.com>
2026-06-12 15:48:01 +0200

    Stop tracking sql scripts

--- sql-scripts/00_load_tables/02-Display.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM patients LIMIT 15;
-SELECT * FROM admissions LIMIT 15;
-SELECT * FROM diagnoses_icd LIMIT 15;
-SELECT * FROM d_icd_diagnoses LIMIT 15;
-
-
-SELECT * FROM discharge LIMIT 15;
